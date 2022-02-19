0dcc36fbb811eef0be4eb25656a279c2c07aa3c3
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-02-18 18:39:59 -0800

    homework 1.1 90% finished

--- dpl_homework1/homework.js
+++ dpl_homework1/homework.js
@@ -27,13 +27,11 @@
 
 
 // for (var x=0; x<=10; x++) {
-//     if (x ===0) {console.log (x + 'is even')
-//     }
-//     else if (x % 2 ===0) {
-//         console.log (x + 'is even')
+//      if (x % 2 ==0) {
+//         console.log (x + ' is even')
 //     }
 //     else {
-//         console.log(x + 'is odd')
+//         console.log(x + ' is odd')
 //     }
 // }
 
@@ -45,7 +43,7 @@
 // while (count <= 10) {
 //     console.log(count);
 //     count +=1;
-
+// }
 
 
 // 5. Look closely at the code below
@@ -94,16 +92,23 @@ if(isOdd(6)){
 //   }
 // }
 // count(24)
+// count(500)
 
 // 2. Write a function that takes an array and returns the sum of all the numbers
 
 // const array = [1, 5, 10, 20, 40];
+// const array2 = [1, 5, 10, 20, 40, 1000000,]
 // let sum = 0;
+// let sum2 = 0
 
 // for (let i = 0; i < array.length; i++) {
 //     sum += array[i];
 // }
+// for (let x = 0; x < array2.length; x++) {
+//     sum2 += array2[x]
+// }
 // console.log(sum);
+// console.log(sum2)
 
 // 3. Write a function that returns the number of vowels in string
 //------------------------------------------------------------------------------
@@ -120,11 +125,34 @@ if(isOdd(6)){
 //     return count
 //   }
 // console.log(numOfVowels('DevPointLabs'))
+// console.log(numOfVowels('HowManyVowelsDoYouThinkIsInThisCRAZYLongSentence'))
 
 // 4. Write a function that takes a math operator, as a string, ('*','/','+','-') and two numbers and returns the result
 //------------------------------------------------------------------------------
 
-
+// const calculator = (opp, num1, num2) =>{
+//     if (opp ==='+') {
+//         return num1 + num2
+//     }
+//     else if (opp ==='-') {
+//         return num1 - num2
+//     }
+//     else if (opp ==='*') {
+//         return num1 * num2
+//     }
+//     else if (opp ==='/') {
+//         return num1 / num2
+//     }
+//     else if (opp ==='%') {           //BONUS :)
+//         return num1 % num2
+//     }
+// }
+// console.log(calculator('+', 25, 5))
+// console.log(calculator('-', 1028373, 10000))
+// console.log(calculator('*', 25, 5))
+// console.log(calculator('/', 100, 10))
+// console.log(calculator('%', 25, 5))
+// console.log(calculator('+', 1028373, 10000))
 
 // 5. Write a function that takes an array of strings and a string and returns true or false if the string is in the array.
 //------------------------------------------------------------------------------
@@ -133,16 +161,15 @@ if(isOdd(6)){
 
 // 6. write a function that takes a person object {firstname: 'joe', lastname: 'shmoe', age: 50} and returns a formatted string "joe shmoe is 50 years old"
 //------------------------------------------------------------------------------
+// const person= {firstname:'randy', lastname:'clements', age:'23'}
 
-// let randy = {name: "Randy Clements", age: 23};
-// let tami = {name: "Tami Clements", age: 62};
-// let tori = {name: "Tori Clements",age: 26};
-// let tiana = {name: "Tiana Clements", age: 34};
+// const formattedString = (info) =>{
+//     return `${info.firstname} ${info.lastname} is ${info.age} years old`
+// }
+// console.log (formattedString(person))
 
-// console.log(randy.name + " is " + randy.age + " years old!");
-// console.log(tami.name + " is " + tami.age + " years old!");
-// console.log(tiana.name + " is " + tiana.age + " years old!");
-// console.log(tori.name + " is " + tori.age + " years old!");
+// const mom= {firstname:'tami', lastname:'clements', age:'64'}
+// console.log (formattedString(mom))
 
 // 7. write a function that takes an array that removes the first and last items and return that new array
 //------------------------------------------------------------------------------
@@ -166,18 +193,40 @@ if(isOdd(6)){
 
 // 8. write a function that swaps the last and first items of an array 
 //------------------------------------------------------------------------------
+// let myData = ['a','b','c','d','e'] 
+//     moreData = ['green', 'yellow', 'orange' , 'red']
+//     function swap(input, index_A, index_B) {
+//     let temp = input [index_A];
+//     input [index_A] = input [index_B];
+//     input [index_B] = temp;
+//     return input
+// }
+// // swap(myData, 2, 4); //2='c' 4='e'
+// console.log(swap(myData, 2, 4))
 
+// console.log(swap(myData, 0, 3))
+
+// console.log(swap(moreData, 1, 3))
 
 // 9. write a function that takes a array and a second argument and returns the index in the array of the second argument
 //------------------------------------------------------------------------------
-
-
+// let myData = ['a','b','c','d','e'] 
+//     function find(input, index_A) {
+//         input.indexOf(index_A)
+//         return input.indexOf(index_A)
+//     }
+//     console.log(find(myData, 'c')) //#2 in the mydata array
+    
+//     console.log(find(myData,'a')) //#0 in the mydata array
 
 // 10. write a function that takes a number and uses a ternary to return if the number is odd or even.
 //------------------------------------------------------------------------------
 
-
-
+// const falseOrTrue = (anyNumber) =>{
+//     return anyNumber + 5 === 100 ? 'does equal 100':'does not equal 100'
+// }
+// console.log(falseOrTrue(95))
+// console.log(falseOrTrue(25))
 // 11. Write a function that prints all  numbers from 0 – 10
 //-----------------------------------------------------------------------------
 
@@ -189,3 +238,10 @@ if(isOdd(6)){
   
 //   upTo10(10)
   
+// const upTo100 = (num)=>{
+//     for(let x = 0; x<= num; x++){
+//         console.log(x)
+//         }
+//     }
+  
+//   upTo100(100)
